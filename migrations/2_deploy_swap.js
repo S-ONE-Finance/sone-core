1af9a3bc555591a69166951506408c06e0ad695a
Tung Pham <tungpv@vnext.com.vn>
2021-03-17 13:16:11 +0700

    deploy factory

--- migrations/2_deploy_swap.js
+++ migrations/2_deploy_swap.js
@@ -1,15 +1,15 @@
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 const UniswapV2Router02 = artifacts.require("UniswapV2Router02");
 
-// address of fee setter 
-const feeSetterAddress = "0x88Cac51E652c4eD234aC461880dD0bd714ca5a1f";
+// address of fee setter
+const feeSetterAddress = "0xAe7fD93a1419dee1376c0b9E27C969E85679AFd2";
 // address of WETH
-const wethAddress = "0x71C5C47d7099E22A4A267495658F7eDE8E55364f";
+const wethAddress = "0x070425a10c4132eeaB8cFbc369150A20EFFaC799";
 
-module.exports = async function(deployer) {
-    // deploy factory
-    await deployer.deploy(UniswapV2Factory, feeSetterAddress);
-    // deploy router
-    const factory = await UniswapV2Factory.deployed();
-    await deployer.deploy(UniswapV2Router02, factory.address, wethAddress);
+module.exports = async function (deployer) {
+  // deploy factory
+  await deployer.deploy(UniswapV2Factory, feeSetterAddress);
+  // deploy router
+  const factory = await UniswapV2Factory.deployed();
+  await deployer.deploy(UniswapV2Router02, factory.address, wethAddress);
 };
