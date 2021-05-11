d5c3d94011668a195653cb7228aafaa934c933b2
thanhnv2 <thanhnv2@vnext.com.vn>
2021-05-12 01:35:00 +0700

    update interface sone router

--- contracts/uniswapv2/interfaces/ISoneSwapRouter.sol
+++ contracts/uniswapv2/interfaces/ISoneSwapRouter.sol
@@ -10,4 +10,33 @@ interface ISoneSwapRouter is IUniswapV2Router02 {
         address to,
         uint deadline
     ) external returns (uint[] memory amounts);
+
+    function addLiquidityOneToken(
+        uint amountIn,
+        uint amountAMin,
+        uint amountBMin,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+
+    function addLiquidityOneTokenETHExactETH(
+        uint amountTokenMin,
+        uint amountETHMin,
+        uint amountOutTokenMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+
+    function addLiquidityOneTokenETHExactToken(
+        uint amountIn,
+        uint amountTokenMin,
+        uint amountETHMin,
+        uint amountOutETHMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
 }
