85897646685abf8c23c8ccd6e514b5808ee2cf79
Daniel Pham <sonph@vnext.com.vn>
2021-06-09 00:59:54 +0700

    Refactor testing
    Add liquidity

--- test/constants/exceptions.js
+++ test/constants/exceptions.js
@@ -1,4 +1,4 @@
-module.exports.reasonRevert = {
+module.exports.revertMsg = {
     INSUFFICIENT_A_AMOUNT                       : "UniswapV2Router: INSUFFICIENT_A_AMOUNT",
     SUBTRACTION_OVERFLOW                        : "SafeMath: subtraction overflow.",
     INSUFFICIENT_LIQUIDITY_BURNED               : "UniswapV2: INSUFFICIENT_LIQUIDITY_BURNED",
