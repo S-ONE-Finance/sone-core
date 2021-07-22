6071905cc54fcb40e9b668f0f6af5c20532e83c2
Daniel Pham <sonph@vnext.com.vn>
2021-07-22 16:28:56 +0700

    Update token addresses

--- src/tasks/accounts.ts
+++ src/tasks/accounts.ts
@@ -3,6 +3,7 @@ import { ERC20 } from 'src/types'
 
 import { accountToSigner, tokenNameToAddress, getDecimalizedBalance } from 'src/tasks/utils'
 import erc20 from 'src/abi/ERC-20.json'
+import { BigNumber } from 'ethers'
 
 task('account:list', 'Prints the list of accounts', async (_taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners()
@@ -18,4 +19,16 @@ task('account:balance', 'Get balance of an account')
     const signer = (await accountToSigner(hre, taskArgs.account))?.[0]
     const balance = await hre.ethers.provider.getBalance(signer.address)
     console.log(hre.ethers.utils.formatEther(balance.toString()))
+  })
+
+task('account:transfer', 'Get balance of an account')
+  .addParam('from', `The from's address`)
+  .addParam('to', `The receiver's address`)
+  .addParam('amount', `Amount`)
+  .setAction(async ({ from, to, amount }, hre) => {
+    const [fromSigner, toSigner] = await accountToSigner(hre, from, to)
+    await fromSigner.sendTransaction({
+      to: toSigner.address,
+      value: BigNumber.from(amount),
+    })
   })
